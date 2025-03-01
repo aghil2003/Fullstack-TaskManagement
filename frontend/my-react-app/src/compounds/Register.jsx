18a9bdeabf6086e3620642147a4391d7079e491c
aghil <aghilpuduruthy680623@gmail.com>
2025-03-01 15:24:27 +0530

    4 changeing in backend url

--- frontend/my-react-app/src/compounds/Register.jsx
+++ frontend/my-react-app/src/compounds/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Register.css';
+import './register.css';
 import instance from "../Axios/instance"
 
 const Register = () => {
